56b2dfe2049788a0fcadad540362fed2f75e51d7
yaafka <habalovas7@gmail.com>
2025-01-06 16:10:31 +0300

    fixed test.py

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import pytest
 
-
-# def test_succeed():
-#     assert True
-
-# @pytest.mark.xfail
-# def test_not_succeed():
-#     assert False
+@pytest.mark.xfail(strict = True)
+def test_succeed():
+    assert True
 
 
-# @pytest.mark.skip
-# def test_skipped():
-#     assert False
+@pytest.mark.xfail()
+def test_not_succeed():
+    assert False
 
-@pytest.mark.xfail
-@pytest.mark.parametrize('number', [
-    '1','2','3','4','5','6'])
-def test_check(number):
-    assert number == '6'
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
